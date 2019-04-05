463bdd417b21258d6a58f030c63c713f2a2fd60d
OOPA <zayintopology@gmail.com>
2019-04-05 10:35:48 +0200

    anacube_dual.py v1

--- IRS_CUBISM/anacube_dual.py
+++ IRS_CUBISM/anacube_dual.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from astropy.io import fits
 from astropy import units as u
 from astropy import wcs
@@ -10,22 +13,24 @@ from mpl_toolkits.mplot3d import Axes3D
 """
 ------------------------------ FUNCTION ------------------------------
 """
-def bimapview(x, y, wvl, data, data0, wvlnum, length):
+def bimapview(lx, ly, wvl, data, data0, wvlnum, patch):
 	"""
 	view the map and choose 1 point/region to check spectrum & flux distribution
 
 	--- INPUT ---
-	x            array to create footprint
-	y
-	wvl      
-	data     
-	length       box side length, int type
+	lx           length of map
+	ly           width of map
+	wvl          wavelength
+	data         cube
+	data0        reference cube
+	wvlnum       number of cube slice, int type
+	patch        replot in order to add selected patch
 	--- OUTPUT ---
-	(x0, y0)     chosen point
-	data1        cut data
-	data2        cut data0
+	fig1         current figure
 	"""
 
+	x = np.arange(lx)
+	y = np.arange(ly)
 	fig1 = plt.figure("map", figsize=(14,5))
 	fig1.suptitle("IRS maps at {} micron".format(wvl[wvlnum]))
 	cnorm = None # Linear
@@ -45,153 +50,87 @@ def bimapview(x, y, wvl, data, data0, wvlnum, length):
 	ax12.set_title("sh")
 	ax12.set_xlabel("x")
 	ax12.set_ylabel("y")
+	if np.size(patch)==4:
+		## add rectangle patch
+		plt.gca().add_patch(plt.Rectangle(
+			(patch[0], patch[1]), # (xmin, ymin)
+			patch[2]-patch[0], patch[3]-patch[1], # width, height
+    		edgecolor='red', linestyle='-', fill=False))
+	elif np.size(patch)==3:
+		## add circle patch
+		plt.gca().add_patch(plt.Circle(
+			(patch[0], patch[1]), # (x0, y0)
+			patch[2], # radius
+    		edgecolor='red', linestyle='-', fill=False))
 
-	## left click to choose 1 point
-	pts = plt.ginput(n=1, show_clicks=True)
-	(x0, y0) = np.array(pts[0]).astype(int)
-	## fluxes in a square of "length" size centered at the chosen point 
-	data1 = data[:, y0-int((length-1)/2):y0+int(length/2)+1, x0-int((length-1)/2):x0+int(length/2)+1]
-	data2 = data0[:, y0-int((length-1)/2):y0+int(length/2)+1, x0-int((length-1)/2):x0+int(length/2)+1]
-	print("Pixels centered (lowerleft) at ({0}, {1})".format(x0, y0))
+	return fig1
 
-	## plot spectrum at chosen square
+def bispecview(wvl, data1, data2):
+	"""
+	extract spectrum at chosen square
+
+	--- INPUT ---
+	wvl           wavelength
+	data1         box
+	data2         box in ref frame
+	"""
 	fig2 = plt.figure("box spectrum")
 	ax2 = plt.subplot()
-	ax2.plot(wvl, np.nansum(data1, axis=(1,2)), 'c-', linewidth=.5, label='dh')
-	ax2.plot(wvl, np.nansum(data2, axis=(1,2)), 'm-', linewidth=.5, label='sh')
+	ax2.errorbar(wvl, np.nansum(data1, axis=0), 0.,
+		c='k', ls='-', lw=.5, ecolor='r', elinewidth=1., label="data")
+	ax2.errorbar(wvl, np.nansum(data2, axis=0), 0.,
+		c='b', ls='-', lw=.5, ecolor='r', elinewidth=1., label="ref")
 	ax2.set_title("Spectra")
 	ax2.set_xlabel('Wavelength (micron)')
-	ax2.set_ylabel(r'$F_{\nu} (MJy)$')
+	ax2.set_ylabel(r'$F_{\nu} (MJy/sr)$')
 #	ax2.set_yscale('log', nonposy='clip')
-	ax2.legend(loc='best')
-#	fig2.savefig('spectra2.png')
+	ax2.legend(loc='upper left')
+#	fig2.savefig('spectrum.png')
 
-	"""
-	box statistics
-	"""
-	## plot flux distribution for pixels in the box
-	fig3 = plt.figure("box flux distribution")
-	ax3 = plt.subplot()
-	# ----- target -----
-	num, bins, patches = ax3.hist(data1[wvlnum,:,:].reshape(-1), bins=int(length*length/4),
-		alpha=.3, lw=.5, edgecolor='w', color='c')
-	bins_c = []
-	for i in range(np.size(bins)-1):
-		bins_c.append((bins[i]+bins[i+1])/2.)
-	bins_c = np.array(bins_c)
-	# ----- ref -----
-	num0, bins0, patches0 = ax3.hist(data2[wvlnum,:,:].reshape(-1), bins=int(length*length/4),
-		alpha=.3, lw=.5, edgecolor='w', color='m')
-	bins0_c = []
-	for i in range(np.size(bins0)-1):
-		bins0_c.append((bins0[i]+bins0[i+1])/2.)
-	bins0_c = np.array(bins0_c)
-
-	## Fit the data using a Gaussian
-	g_init = models.Gaussian1D(amplitude=1., mean=0, stddev=1.)
-	fit_g = fitting.LevMarLSQFitter()
-	# ----- target -----
-	g = fit_g(g_init, bins_c, num)
-	numsum = 0
-	for j in range(np.size(num)):
-		if (bins_c[j]>g.mean-g.stddev and bins_c[j]<g.mean+g.stddev):
-			numsum += num[j]
-	sig1 = float(numsum / (length*length))
-	print("box flux mean: ", g.mean*1.)
-	print("1-sigma percentage: {:.0%}".format(sig1))
-	ax3.plot(bins_c, g(bins_c), c='c')
-	ax3.axvline(g.mean-g.stddev, c='c', linestyle=':')
-	ax3.axvline(g.mean+g.stddev, c='c', linestyle=':')
-	# ----- ref -----
-	g0 = fit_g(g_init, bins0_c, num0)
-	num0sum = 0
-	for j in range(np.size(num0)):
-		if (bins0_c[j]>g0.mean-g0.stddev and bins0_c[j]<g0.mean+g0.stddev):
-			num0sum += num0[j]
-	sig1ref = float(num0sum / np.nansum(num))
-	print("ref box flux mean: ", g0.mean*1.)
-	print("ref 1-sigma percentage: {:.0%}".format(sig1ref))
-	ax3.plot(bins0_c, g0(bins0_c), c='m')
-	ax3.axvline(g0.mean-g0.stddev, c='m', linestyle=':')
-	ax3.axvline(g0.mean+g0.stddev, c='m', linestyle=':')
-
-	ax3.set_title("Flux distribution")
-	ax3.set_xlabel(r'$F_{\nu} (MJy)$')
-	ax3.set_ylabel("Number of pixels")
-
-	plt.show()
-
-	return x0, y0, data1, data2
-
-def cal_res(wvl, data1, data2, length):
+def cal_res(wvl, data1, data2, trim):
 	"""
 	calculate residuals for chosen pixels with each wavelength
 
 	--- INPUT ---
 	wvl       
-	data1     
-	data2     
-	length       int type
+	data1        data (2D)
+	data2        ref
+	trim         alert trim
 	--- OUTPUT ---
-	res          3D array
+	res          1D array
+	res_std      std_dev of res
+	npix         nb of pixels used in the calculation
+	badv         wvl of slice which might have bad pixels ()
 	"""
-	NX = length
-	NY = length
-	NZ = np.size(wvl)
-	res = np.zeros(NX*NY*NZ).reshape(NZ, NY, NX) # create a 3D array
-	for i in range(NX):
-		for j in range(NY):
-			F_dh = data1[:,j,i]
-			F_sh = data2[:,j,i]
-			for k in range(NZ):
-				if np.isnan(F_dh[k])==0 and np.isnan(F_sh[k])==0:
-					if F_sh[k]==0.:
-						res[k,j,i] = np.nan
-					else:
-						resk = (F_dh[k] - F_sh[k]) * 2. / (F_dh[k] + F_sh[k])
-						res[k,j,i] = resk
-				else:
-					res[k,j,i] = np.nan
+	npix = np.size(data1[:,0])
+	print("npix = ", npix)
+	nv = np.size(wvl)
+	res = np.zeros(nv, dtype=float)
+	badv = []
+	for k in range(nv):
+		F_dh = np.nansum(data1[:,k])
+		F_sh = np.nansum(data2[:,k])
+		if F_sh==0.:
+			res[k] = 0.
+		else:
+			res[k] = (F_dh - F_sh) / F_sh
+	## alert
+		if abs(res[k])>trim:
+			badv.append(wvl[k])
+			print("Need to review {:.5} micron slice".format(wvl[k]))
+	print(badv)
+	## res_std (par rapport a wvl)
+	res_std = rms(res)
+	## plot res - wvl
+	fig3 = plt.figure("Residual Distribution")
+	ax3 = plt.subplot()
+	ax3.plot(wvl, res, 'k-', lw=.5, label="std_dev = {:.2%}".format(res_std))
+	ax3.set_xlabel('Wavelength (micron)')
+	ax3.set_ylabel('Residual')
+#	ax3.set_xscale('log', nonposy='clip')
+	ax3.legend(loc='upper right')
 
 	return res
-
-def spec_res(x0, y0, wvl, res):
-	"""
-	plot res - wvl & res_std. - wvl
-
-	--- INPUT ---
-	(x0, y0)     position in pixel coord
-	wvl          NAXIS3 elements
-	res          3D array
-	"""
-	fig = plt.figure("res & res_std distribution", figsize=(14,5))
-	## res - wvl
-	res_wvl = np.nanmean(res, axis=(1,2))
-	res_wvl_mean = np.nanmean(res_wvl)
-	print("res_wvl_mean = ", res_wvl_mean)
-	ax1 = plt.subplot(121)
-	ax1.plot(wvl, 0.*wvl, 'k-', linewidth=.5)
-#	ax1.plot(wvl, res_wvl_mean, 'k-', linewidth=.5)
-	ax1.plot(wvl, res_wvl, 'b.', markersize=.8)
-	ax1.set_xlabel("Wavelength (um)")
-	ax1.set_ylabel("Residual")
-	ax1.set_ylim(-3, 3)
-	## res_std - wvl
-	std_wvl = []
-	for k in range(np.size(wvl)):
-		std_wvl.append(rms(res[k,:,:]))
-	std_wvl_mean = np.nanmean(std_wvl)
-	print("std_wvl_mean = ", std_wvl_mean)
-	ax2 = plt.subplot(122)
-	ax2.plot(wvl, 0.*wvl, 'k-', linewidth=.5)
-#	ax2.plot(wvl, std_wvl_mean, 'k-', linewidth=.5)
-	ax2.plot(wvl, std_wvl, 'b.', markersize=.8)
-	ax2.set_xlabel("Wavelength (um)")
-	ax2.set_ylabel("Residual std")
-	ax2.set_ylim(-3, 3)
-
-#	fig.savefig('res/({0}, {1}).png'.format(x0, y0))
-	plt.show()
 
 def rms(a):
 	"""
@@ -210,9 +149,14 @@ def rms(a):
 """
 ------------------------------ MAIN ------------------------------
 """
-filename = 'dh_SL2_cube'
-filename0 = 'sh_SL2_cube' 
+filename = '1_LL2_cube'
+filename0 = 'n83_84_LL2_cube' 
 #filename0 = filename # set 2 filenames the same if no need to compare cubes
+## select fixed rectangle: lower left (x1, y1) & upper right (x2, y2)
+#xmin, xmax = 50, 70
+#ymin, ymax = 65, 80
+xmin, xmax = 52, 54
+ymin, ymax = 67, 69
 
 with fits.open(filename+'.fits') as hdul:
 	data = hdul[0].data
@@ -222,23 +166,96 @@ with fits.open(filename+'.fits') as hdul:
 	NAXIS3 = hdr['NAXIS3']
 	wvl = hdul[1].data[0][0][:,0]
 #	wvl = hdul[1].data # for rewitten header
-with fits.open(filename0+'.fits') as hdul0:
-	data0 = hdul0[0].data
+hdul0 = fits.open(filename0+'.fits')
+data0 = hdul0[0].data
 
 while True:
-	flag = input("(Re)Analyse? (0 - yes / else - no) ") # keyboard input 1
-	if (flag=="0"):
-		wvlnum = int(input("Choose a slice of wavelength: ")) # keyboard input 2
+	b1 = input("(Re)Analyse? (0 - yes / else - no) ") # keyboard input 1
+	if b1=="0":
+		wvlnum = int(input("Select a slice of wavelength (0 - {}): ".format(NAXIS3-1))) # keyboard input 2
 		print("wavelength = ", wvl[wvlnum])
-		length = int(input("Choose the square length: ")) # keyboard input 3
-		x = np.arange(hdr['NAXIS1'])
-		y = np.arange(hdr['NAXIS2'])
-		x0, y0, data1, data2 = bimapview(x, y, wvl, data, data0, wvlnum, length)
+
+		## view map 
+		figmap = bimapview(NAXIS1, NAXIS2, wvl, data, data0, wvlnum, 0)
 		"""
-		compare two cubes [comment the following if not compare]
+		box statistics
 		"""
-#		res = cal_res(wvl, data1, data2, length)
-#		print("res shape = ", res.shape)
-#		spec_res(x0, y0, wvl, res)
+		## select box mode
+		b0 = input("Select mode (0 - fixed / 1 - clicked / 2 - circle / else - skip) ") # keyboard input 0
+		print("...")
+		if b0=="0":
+			## fixed box 
+			xb, xt = xmin, xmax # b-bottom, t-top
+			yb, yt = ymin, ymax
+			plt.close(figmap)
+		elif b0=="1":
+			## left click to select 2 points which determine a rectangle by the diagonal
+			pts = figmap.ginput(n=2, show_clicks=True)
+			(x1, y1) = np.array(pts[0]).astype(int)
+			(x2, y2) = np.array(pts[1]).astype(int)
+			xb, xt = min(x1, x2), max(x1, x2)
+			yb, yt = min(y1, y2), max(y1, y2)
+			plt.close(figmap)
+		elif b0=="2":
+			## left click to select 1 point which determine a circle
+			radius = int(input("Select circle radius (only int type): ")) # keyboard input 3
+			pts = figmap.ginput(n=1, show_clicks=True)
+			(x0, y0) = np.array(pts[0]).astype(int)
+			plt.close(figmap)
+		else:
+			plt.close(figmap)
+			continue
+		
+		plt.show()
+
+		## extract the flux
+		if not(b0=="2"):
+			print("rectangle diagonal ({}, {}) - ({}, {})".format(xb, yb, xt, yt))
+			## add patch onto map
+			patch = [xb, yb, xt, yt]
+			bimapview(NAXIS1, NAXIS2, wvl, data, data0, wvlnum, patch)
+			## fluxes in box 
+			data1 = data[:, yb:yt, xb:xt].reshape(((xt-xb)*(yt-yb), NAXIS3))
+			data2 = data0[:, yb:yt, xb:xt].reshape(((xt-xb)*(yt-yb), NAXIS3))
+		elif b0=="2":
+			print("circle centered at ({0}, {1})".format(x0, y0))
+			## add patch onto map
+			patch = [x0, y0, radius]
+			bimapview(NAXIS1, NAXIS2, wvl, data, data0, wvlnum, patch)
+			## fluxes in box 
+			data1 = [data[:, y0, x0]]
+			data2 = [data0[:, y0, x0]]
+			for dx in range(radius+1):
+				for dy in range(radius+1):
+					p2 = dx*dx + dy*dy
+					r2 = radius*radius
+					if (dx==0 and dy==0):
+						continue
+					elif (dx==0 or dy==0):
+						#print("dx, dy ", dx, dy)
+						data1.append(data[:, y0-dy, x0-dx])
+						data1.append(data[:, y0+dy, x0+dx])
+						data2.append(data0[:, y0-dy, x0-dx])
+						data2.append(data0[:, y0+dy, x0+dx])
+					elif (p2!=0 and p2<=r2):
+						#print("dx, dy ", dx, dy)
+						data1.append(data[:, y0-dy, x0-dx])
+						data1.append(data[:, y0-dy, x0+dx])
+						data1.append(data[:, y0+dy, x0-dx])
+						data1.append(data[:, y0+dy, x0+dx])
+						data2.append(data0[:, y0-dy, x0-dx])
+						data2.append(data0[:, y0-dy, x0+dx])
+						data2.append(data0[:, y0+dy, x0-dx])
+						data2.append(data0[:, y0+dy, x0+dx])
+			data1 = np.array(data1)
+			data2 = np.array(data2)
+			print("(nb of pix in circle, NAXIS3) = ", data1.shape)
+
+		## view spectrum
+		bispecview(wvl, data1, data2)
+		## compare two cubes [comment the following if not compare]
+		res = cal_res(wvl, data1, data2, .5)
+
+		plt.show()
 	else:
 		exit(0)
